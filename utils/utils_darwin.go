4ace6deb341f12c06ce2c28627626f31c2046d52
lihe <cranelee13@outlook.com>
2020-06-20 23:20:01 +0800

    remove go-openssl package

--- utils/utils_darwin.go
+++ utils/utils_darwin.go
@@ -5,12 +5,10 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/sha1"
-	"fmt"
 	"hack-browser-data/log"
 	"os/exec"
 	"path/filepath"
 
-	"github.com/forgoer/openssl"
 	"golang.org/x/crypto/pbkdf2"
 )
 
@@ -26,12 +24,12 @@ var (
 	chromePass []byte
 )
 
-func GetDBPath(dbName string) string {
+func GetDBPath(dbName string) (string, error) {
 	s, err := filepath.Glob(macChromeDir + dbName)
 	if err != nil && len(s) == 0 {
-		panic(err)
+		return "", err
 	}
-	return s[0]
+	return s[0], nil
 }
 
 func InitChromeKey() {
@@ -47,78 +45,33 @@ func InitChromeKey() {
 		log.Println(err)
 		panic(err)
 	}
-
 	if stderr.Len() > 0 {
 		panic(stderr.String())
 	}
 	// replace /n
 	temp := stdout.Bytes()
 	chromePass = temp[:len(temp)-1]
+	DecryptPass(chromePass)
+}
+
+func DecryptPass(chromePass []byte) {
 	chromeKey = pbkdf2.Key(chromePass, chromeSalt, 1003, 16, sha1.New)
 }
 
-func DecryptPass(chromePass []byte) []byte {
-	l := pbkdf2.Key(chromePass, chromeSalt, 1003, 16, sha1.New)
-	return l
-}
-
-func Aes128CBCDecrypt(encryptPass []byte) string {
-	src, err := openssl.AesCBCDecrypt(encryptPass, chromeKey, iv, openssl.PKCS5_PADDING)
+func Aes128CBCDecrypt(encryptPass []byte) (string, error) {
+	block, err := aes.NewCipher(chromeKey)
 	if err != nil {
-		log.Println(err)
+		return "", err
 	}
-	return string(src)
+	dst := make([]byte, len(encryptPass))
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(dst, encryptPass)
+	dst = PKCS5UnPadding(dst)
+	return string(dst), nil
 }
 
-func AesDecrypt(ciphertext []byte, key, iv []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	blockMode := cipher.NewCBCDecrypter(block, iv)
-	origData := make([]byte, len(ciphertext))
-	fmt.Println(blockMode.BlockSize())
-	//func (x *cbcDecrypter) CryptBlocks(dst, src []byte) {
-	//	if len(src)%x.blockSize != 0 {
-	//		panic("crypto/cipher: input not full blocks")
-	blockMode.CryptBlocks(origData, ciphertext)
-	origData = PKCS5UnPadding(origData)
-	return origData, nil
-}
-
-func ZeroPadding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{0}, padding)
-	return append(ciphertext, padtext...)
-}
-
-func ZeroUnPadding(origData []byte) []byte {
-	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
-}
-
-func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
-}
-
-func PKCS5UnPadding(origData []byte) []byte {
-	length := len(origData)
-	// 去掉最后一个字节 unpadding 次
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
-}
-
-func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
-	padding := blockSize - len(ciphertext)%blockSize
-	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(ciphertext, padtext...)
-}
-
-func PKCS7UnPadding(origData []byte) []byte {
-	length := len(origData)
-	unpadding := int(origData[length-1])
-	return origData[:(length - unpadding)]
+func PKCS5UnPadding(src []byte) []byte {
+	length := len(src)
+	unpadding := int(src[length-1])
+	return src[:(length - unpadding)]
 }
