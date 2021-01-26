dba3bac5339665e5e0f73b7878e94652b902fd56
Carlo Mandelli <camandel@gmail.com>
2021-01-26 10:54:49 +0100

    feat: add Chromium support for Linux

--- core/browser.go
+++ core/browser.go
@@ -14,6 +14,7 @@ import (
 const (
 	chromeName     = "Chrome"
 	chromeBetaName = "Chrome Beta"
+	chromiumName   = "Chromium"
 	edgeName       = "Microsoft Edge"
 	firefoxName    = "Firefox"
 	speed360Name   = "360speed"
